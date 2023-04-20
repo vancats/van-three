b706461cfd1f3bb81e945a92b5f260c65602d7ac
vancats <leiqifan@163.com>
2023-04-20 23:11:47 +0800

    feat: galaxy

--- src/main.js
+++ src/main.js
@@ -2,171 +2,183 @@ import './style.css'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 import * as dat from 'dat.gui'
-import vertexShader from './shaders/04.galaxy/vertex.glsl'
-import fragmentShader from './shaders/04.galaxy/fragment.glsl'
-
-const gui = new dat.GUI({ width: 360 })
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
+import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader'
 
 const canvas = document.querySelector('#webgl')
 const sizes = { width: window.innerWidth, height: window.innerHeight }
 const aspectRadio = sizes.width / sizes.height
+const gui = new dat.GUI({ closed: false })
+const parametres = {}
 
-window.addEventListener('resize', () => {
-    // Update Sizes
-    sizes.width = window.innerWidth
-    sizes.height = window.innerHeight
+/// Renderer
+const renderer = new THREE.WebGLRenderer({ canvas })
+renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+renderer.shadowMap.enabled = true
+renderer.shadowMap.type = THREE.PCFSoftShadowMap
+// 启用后可以得到和其他软件（如 Blender）中同样的光照情况
+renderer.physicallyCorrectLights = true
+renderer.outputEncoding = THREE.sRGBEncoding // 默认是 LinearEncoding
+renderer.toneMapping = THREE.ReinhardToneMapping
+renderer.toneMappingExposure = 3 // 曝光
 
-    // Update Camera
-    camera.aspect = sizes.width / sizes.height
-    camera.updateProjectionMatrix()
-
-    // Update Renderer
-    renderer.setSize(sizes.width, sizes.height)
-    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+gui.add(renderer, 'toneMapping', {
+    No: THREE.NoToneMapping,
+    Liner: THREE.LinearToneMapping,
+    Reinhard: THREE.ReinhardToneMapping,
+    Cineon: THREE.CineonToneMapping,
+    ACESFilmic: THREE.ACESFilmicToneMapping,
+}).onFinishChange(() => {
+    // 直接转换过来的是字符串。需要转成数字
+    renderer.toneMapping = Number(renderer.toneMapping)
+    updateAllMaterials()
 })
+gui.add(renderer, 'toneMappingExposure').min(0).max(10).step(0.001)
 
 const scene = new THREE.Scene()
 
-const parameters = {}
-parameters.count = 100000
-parameters.size = 0.01
-parameters.radius = 5
-parameters.branches = 4
-parameters.spin = 1
-parameters.randomness = 0.2
-parameters.randomnessPower = 3
-parameters.insideColor = 0xff6030
-parameters.outsideColor = 0x1b3984
-
-let geometry
-let material
-let points
-
-/// Texture
-const textureLoader = new THREE.TextureLoader()
-
-
-/// Galaxy
-function generateGalaxy() {
-    if (points) {
-        geometry.dispose()
-        material.dispose()
-        scene.remove(points)
-    }
-
-    const {
-        count, size, radius, branches, spin, randomness,
-        randomnessPower, insideColor, outsideColor
-    } = parameters
-
-    geometry = new THREE.BufferGeometry()
-    const positions = new Float32Array(count * 3)
-    const colors = new Float32Array(count * 3)
-
-    const colorInside = new THREE.Color(insideColor)
-    const colorOutside = new THREE.Color(outsideColor)
-
-    for (let i = 0; i < count; i++) {
-        const i3 = i * 3
-
-        // Positions
-        const randomRadius = Math.random() * radius
-        const branchAngle = (i % branches) / branches * Math.PI * 2
-        const spinAngle = randomRadius * spin
-
-        const randomX = Math.pow(Math.random(), randomnessPower) * randomness * (Math.random() < 0.5 ? 1 : -1) * Math.random() * radius
-        const randomY = Math.pow(Math.random(), randomnessPower) * randomness * (Math.random() < 0.5 ? 1 : -1) * Math.random() * radius
-        const randomZ = Math.pow(Math.random(), randomnessPower) * randomness * (Math.random() < 0.5 ? 1 : -1) * Math.random() * radius
-
-        positions[i3] = Math.cos(branchAngle + spinAngle) * randomRadius + randomX
-        positions[i3 + 1] = randomY
-        positions[i3 + 2] = Math.sin(branchAngle + spinAngle) * randomRadius + randomZ
-
-
-        // Colors
-
-        const mixedColor = colorInside.clone()
-        // 会改变原来的 Color
-        mixedColor.lerp(colorOutside, randomRadius / radius)
-        colors[i3] = mixedColor.r
-        colors[i3 + 1] = mixedColor.g
-        colors[i3 + 2] = mixedColor.b
-
-    }
-    geometry.setAttribute(
-        'position',
-        new THREE.BufferAttribute(positions, 3)
-    )
-
-    geometry.setAttribute(
-        'color',
-        new THREE.BufferAttribute(colors, 3)
-    )
-
-    material = new THREE.ShaderMaterial({
-        vertexShader,
-        fragmentShader,
-        depthWrite: false,
-        blending: THREE.AdditiveBlending,
-        vertexColors: true,
-    })
-    points = new THREE.Points(geometry, material)
-    scene.add(points)
-}
-generateGalaxy()
-
-
-
-
-/// DEBUG
-gui.add(parameters, 'count').min(100).max(1000000).step(100).onFinishChange(generateGalaxy)
-gui.add(parameters, 'size').min(0.001).max(0.1).step(0.001).onFinishChange(generateGalaxy)
-gui.add(parameters, 'radius').min(0.01).max(20).step(0.01).onFinishChange(generateGalaxy)
-gui.add(parameters, 'branches').min(2).max(20).step(1).onFinishChange(generateGalaxy)
-gui.add(parameters, 'spin').min(-5).max(5).step(0.001).onFinishChange(generateGalaxy)
-gui.add(parameters, 'randomness').min(0).max(2).step(0.001).onFinishChange(generateGalaxy)
-gui.add(parameters, 'randomnessPower').min(1).max(10).step(0.001).onFinishChange(generateGalaxy)
-gui.addColor(parameters, 'insideColor').onFinishChange(generateGalaxy)
-gui.addColor(parameters, 'outsideColor').onFinishChange(generateGalaxy)
-
-
-
-
-
-
 /// Camera
-// 角度 + 宽高比 + 近远平面
-const camera = new THREE.PerspectiveCamera(75, aspectRadio, 0.1, 100)
-camera.position.z = 4
-camera.position.y = 2
+const camera = new THREE.PerspectiveCamera(35, aspectRadio, 0.1, 100)
+camera.position.z = 7
+camera.position.y = 8
+camera.position.x = 8
 scene.add(camera)
 
 /// Controls
 const controls = new OrbitControls(camera, canvas)
 controls.enableDamping = true
 
-const renderer = new THREE.WebGLRenderer({ canvas })
-renderer.setSize(sizes.width, sizes.height)
-renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
+/// Texture
+const cubeTextureLoader = new THREE.CubeTextureLoader()
+const environmentMap = cubeTextureLoader.load([
+    '/textures/environmentMaps/0/px.jpg',
+    '/textures/environmentMaps/0/nx.jpg',
+    '/textures/environmentMaps/0/py.jpg',
+    '/textures/environmentMaps/0/ny.jpg',
+    '/textures/environmentMaps/0/pz.jpg',
+    '/textures/environmentMaps/0/nz.jpg',
+])
+environmentMap.encoding = THREE.sRGBEncoding // 除了渲染器，环境贴图也需要加上
+scene.background = environmentMap // 仅覆盖背景
+scene.environment = environmentMap // 覆盖所有物体
+
+
+/// Objects
+const updateAllMaterials = () => {
+    scene.traverse(child => {
+        if (child instanceof THREE.Mesh && child.material instanceof THREE.MeshStandardMaterial) {
+            child.material.envMapIntensity = parametres.envMapIntensity
+            child.material.needsUpdate = true
+
+            child.castShadow = true
+            child.receiveShadow = true
+        }
+    })
+}
+parametres.envMapIntensity = 5
+gui.add(parametres, 'envMapIntensity').min(0).max(40).step(0.001).onChange(updateAllMaterials)
+
+
+
+const plane = new THREE.Mesh(
+    new THREE.PlaneBufferGeometry(30, 30),
+    new THREE.MeshStandardMaterial({
+        color: 0x777777,
+        metalness: 0.3,
+        roughness: 0.4,
+    })
+)
+plane.position.y = -5
+plane.rotation.x = -Math.PI * 0.5
+scene.add(plane)
+
+const textureLoader = new THREE.TextureLoader()
+const mapTexture = textureLoader.load('/models/LeePerrySmith/color.jpg')
+mapTexture.encoding = THREE.sRGBEncoding
+
+const normalTexture = textureLoader.load('/models/LeePerrySmith/normal.jpg')
+
+const material = new THREE.MeshStandardMaterial({
+    map: mapTexture,
+    normalMap: normalTexture,
+})
+
+/// Models
+const gltfLoader = new GLTFLoader()
+const dracoLoader = new DRACOLoader()
+dracoLoader.setDecoderPath('/draco/')
+gltfLoader.setDRACOLoader(dracoLoader)
+
+gltfLoader.load(
+    '/models/LeePerrySmith/LeePerrySmith.glb',
+    (gltf) => {
+        const mesh = gltf.scene.children[0]
+        mesh.rotation.y = Math.PI * 0.5
+        mesh.material = material
+        scene.add(mesh)
+
+        updateAllMaterials()
+    }
+)
+
+
+
+
+
+
+/// Lights
+const directionalLight = new THREE.DirectionalLight(0xffffff, 3)
+directionalLight.position.set(1, 2, -2)
+
+scene.add(directionalLight)
+
+const directionalLightHelper = new THREE.CameraHelper(directionalLight.shadow.camera)
+scene.add(directionalLightHelper)
+
+
+/// Shadow
+plane.receiveShadow = true
+directionalLight.castShadow = true
+directionalLight.shadow.mapSize.set(1024, 1024)
+directionalLight.shadow.camera.far = 15
+directionalLight.shadow.camera.left = - 7
+directionalLight.shadow.camera.top = 7
+directionalLight.shadow.camera.right = 7
+directionalLight.shadow.camera.bottom = - 7
+directionalLight.shadow.normalBias = 0.05
+
+/// GUI
+gui.add(directionalLight, 'intensity').min(0).max(10).step(0.001).name('LightIntensity')
+gui.add(directionalLight.position, 'x').min(-5).max(5).step(0.001).name('LightX')
+gui.add(directionalLight.position, 'y').min(-5).max(5).step(0.001).name('LightY')
+gui.add(directionalLight.position, 'z').min(-5).max(5).step(0.001).name('LightZ')
+
+
+/// Animations
 const clock = new THREE.Clock()
-
+let prevElapsedTime = 0
 const tick = () => {
+
     const elapsedTime = clock.getElapsedTime()
-    // points.rotation.y = elapsedTime * 0.2
+    const deltaTime = elapsedTime - prevElapsedTime
+    prevElapsedTime = elapsedTime
 
-
-    /// 性能更好的操作是使用 shadar 来进行批量操作
-    // for (let i = 0; i < parameters.count; i++) {
-    //     const i3 = i * 3
-    //     const x = geometry.attributes.position.array[i3]
-    //     geometry.attributes.position.array[i3 + 1] += (Math.random() - 0.5)
-    // }
-
-    // 如果使用了 damping，必须要在这里更新
     controls.update()
     renderer.render(scene, camera)
     requestAnimationFrame(tick)
 }
 
 tick()
+
+window.addEventListener('resize', () => {
+    sizes.width = window.innerWidth
+    sizes.height = window.innerHeight
+
+    camera.aspect = sizes.width / sizes.height
+    camera.updateProjectionMatrix()
+
+    renderer.setSize(sizes.width, sizes.height)
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+})
---
+++ src/shaders/04.galaxy/index.js
@@ -0,0 +1,191 @@
+import './style.css'
+import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import * as dat from 'dat.gui'
+import vertexShader from './shaders/04.galaxy/vertex.glsl'
+import fragmentShader from './shaders/04.galaxy/fragment.glsl'
+
+const gui = new dat.GUI({ width: 360 })
+
+const canvas = document.querySelector('#webgl')
+const sizes = { width: window.innerWidth, height: window.innerHeight }
+const aspectRadio = sizes.width / sizes.height
+
+const scene = new THREE.Scene()
+
+/// Camera
+// 角度 + 宽高比 + 近远平面
+const camera = new THREE.PerspectiveCamera(75, aspectRadio, 0.01, 100)
+camera.position.z = 4
+camera.position.y = 2
+scene.add(camera)
+
+/// Controls
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true
+
+const renderer = new THREE.WebGLRenderer({ canvas })
+renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+window.addEventListener('resize', () => {
+    // Update Sizes
+    sizes.width = window.innerWidth
+    sizes.height = window.innerHeight
+
+    // Update Camera
+    camera.aspect = sizes.width / sizes.height
+    camera.updateProjectionMatrix()
+
+    // Update Renderer
+    renderer.setSize(sizes.width, sizes.height)
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+})
+
+
+const parameters = {}
+parameters.count = 100000
+parameters.size = 0.01
+parameters.radius = 5
+parameters.branches = 4
+parameters.spin = 1
+parameters.randomness = 0.2
+parameters.randomnessPower = 3
+parameters.insideColor = 0xff6030
+parameters.outsideColor = 0x1b3984
+
+let geometry
+let material
+let points
+
+/// Texture
+const textureLoader = new THREE.TextureLoader()
+
+
+/// Galaxy
+function generateGalaxy() {
+    if (points) {
+        geometry.dispose()
+        material.dispose()
+        scene.remove(points)
+    }
+
+    const {
+        count, size, radius, branches, spin, randomness,
+        randomnessPower, insideColor, outsideColor
+    } = parameters
+
+    geometry = new THREE.BufferGeometry()
+    const positions = new Float32Array(count * 3)
+    const colors = new Float32Array(count * 3)
+    const scales = new Float32Array(count * 1)
+    const randomnessPosition = new Float32Array(count * 3)
+
+    const colorInside = new THREE.Color(insideColor)
+    const colorOutside = new THREE.Color(outsideColor)
+
+    for (let i = 0; i < count; i++) {
+        const i3 = i * 3
+
+        // Positions
+        const randomRadius = Math.random() * radius
+        const branchAngle = (i % branches) / branches * Math.PI * 2
+
+        const randomX = Math.pow(Math.random(), randomnessPower) * randomness * (Math.random() < 0.5 ? 1 : -1) * randomRadius
+        const randomY = Math.pow(Math.random(), randomnessPower) * randomness * (Math.random() < 0.5 ? 1 : -1) * randomRadius
+        const randomZ = Math.pow(Math.random(), randomnessPower) * randomness * (Math.random() < 0.5 ? 1 : -1) * randomRadius
+
+        randomnessPosition[i3] = randomX
+        randomnessPosition[i3 + 1] = randomY
+        randomnessPosition[i3 + 2] = randomZ
+
+
+        positions[i3] = Math.cos(branchAngle) * randomRadius
+        positions[i3 + 1] = 0
+        positions[i3 + 2] = Math.sin(branchAngle) * randomRadius
+
+
+        // Colors
+
+        const mixedColor = colorInside.clone()
+        // 会改变原来的 Color
+        mixedColor.lerp(colorOutside, randomRadius / radius)
+        colors[i3] = mixedColor.r
+        colors[i3 + 1] = mixedColor.g
+        colors[i3 + 2] = mixedColor.b
+
+        scales[i] = Math.random()
+    }
+    geometry.setAttribute(
+        'position',
+        new THREE.BufferAttribute(positions, 3)
+    )
+
+    geometry.setAttribute(
+        'color',
+        new THREE.BufferAttribute(colors, 3)
+    )
+
+    geometry.setAttribute(
+        'aScale',
+        new THREE.BufferAttribute(scales, 1)
+    )
+
+    geometry.setAttribute(
+        'aRondom',
+        new THREE.BufferAttribute(randomnessPosition, 3)
+    )
+
+    material = new THREE.ShaderMaterial({
+        // size: parameters.size,
+        // sizeAttenuation: true,
+        blending: THREE.AdditiveBlending,
+        depthWrite: false,
+        vertexColors: true,
+        vertexShader,
+        fragmentShader,
+        uniforms: {
+            // 修复像素问题
+            uSize: { value: 30 * renderer.getPixelRatio() },
+            uTime: { value: 0 }
+        }
+    })
+    points = new THREE.Points(geometry, material)
+    scene.add(points)
+}
+generateGalaxy()
+
+
+
+
+/// DEBUG
+gui.add(parameters, 'count').min(100).max(1000000).step(100).onFinishChange(generateGalaxy)
+gui.add(parameters, 'size').min(0.001).max(0.1).step(0.001).onFinishChange(generateGalaxy)
+gui.add(parameters, 'radius').min(0.01).max(20).step(0.01).onFinishChange(generateGalaxy)
+gui.add(parameters, 'branches').min(2).max(20).step(1).onFinishChange(generateGalaxy)
+gui.add(parameters, 'spin').min(-5).max(5).step(0.001).onFinishChange(generateGalaxy)
+gui.add(parameters, 'randomness').min(0).max(2).step(0.001).onFinishChange(generateGalaxy)
+gui.add(parameters, 'randomnessPower').min(1).max(10).step(0.001).onFinishChange(generateGalaxy)
+gui.addColor(parameters, 'insideColor').onFinishChange(generateGalaxy)
+gui.addColor(parameters, 'outsideColor').onFinishChange(generateGalaxy)
+
+
+
+
+
+
+
+const clock = new THREE.Clock()
+
+const tick = () => {
+    const elapsedTime = clock.getElapsedTime()
+
+    material.uniforms.uTime.value = elapsedTime
+
+    // 如果使用了 damping，必须要在这里更新
+    controls.update()
+    renderer.render(scene, camera)
+    requestAnimationFrame(tick)
+}
+
+tick()
